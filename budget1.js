b80c6094c42c8f1d46ec82cb1f172c32a2d952f3
gooduwill <goodyouwill@gmail.com>
2025-08-31 17:56:34 +0530

    changes in budget

--- budget1.js
+++ budget1.js
@@ -10,7 +10,7 @@ function getBudget(arr){
     return result
 }
 console.log(getBudget([
-    { name: "John", age: 22, budget: 23000 },
+    { name: "John", age: 222, budget: 23000 },
     { name: "Steve",  age: 32, budget: 40000 },
     { name: "Martin",  age: 16, budget: 2700 }
   ]))
